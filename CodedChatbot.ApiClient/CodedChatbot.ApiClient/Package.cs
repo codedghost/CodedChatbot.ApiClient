87c60a80c33b0880ac5925d9307194a1627c6186
codedghost2 <codedghost2@gmail.com>
2019-10-20 15:16:13 +0100

    Initial commit

---
+++ CodedChatbot.ApiClient/CodedChatbot.ApiClient/Package.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreCodedChatbot.ApiClient.ApiClients;
+using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreCodedChatbot.ApiClient
+{
+    public static class Package
+    {
+        public static ServiceCollection AddApiClientServices(this ServiceCollection services)
+        {
+            services.AddTransient<IGuessingGameApiClient, GuessingGameApiClient>();
+            services.AddTransient<IPlaylistApiClient, PlaylistApiClient>();
+            services.AddTransient<IStreamStatusClient, StreamStatusClient>();
+            services.AddTransient<IVipApiClient, VipApiClient>();
+
+            return services;
+        }
+    }
+}
