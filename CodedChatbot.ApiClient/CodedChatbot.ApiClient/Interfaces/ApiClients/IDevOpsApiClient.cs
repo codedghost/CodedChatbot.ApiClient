ae7cc4da2cd5e6ef727c161866ecb809ad91c554
codedghost2 <codedghost2@gmail.com>
2020-01-11 11:26:02 +0000

    Adding missing methods to DepOpsApiClient interface and adding new endpoint accessor for Backlog work items

--- CodedChatbot.ApiClient/CodedChatbot.ApiClient/Interfaces/ApiClients/IDevOpsApiClient.cs
+++ CodedChatbot.ApiClient/CodedChatbot.ApiClient/Interfaces/ApiClients/IDevOpsApiClient.cs
@@ -1,6 +1,14 @@
-﻿namespace CoreCodedChatbot.ApiClient.Interfaces.ApiClients
+﻿using System.Threading.Tasks;
+using CoreCodedChatbot.ApiContract.RequestModels.DevOps;
+using CoreCodedChatbot.ApiContract.ResponseModels.DevOps;
+
+namespace CoreCodedChatbot.ApiClient.Interfaces.ApiClients
 {
     public interface IDevOpsApiClient
     {
+        Task<GetWorkItemByIdResponse> GetWorkItemById(int id);
+        Task<GetAllCurrentWorkItemsResponse> GetAllCurrentWorkItems();
+        Task<bool> RaiseBug(RaiseBugRequest raiseBugRequest);
+        Task<GetAllBacklogWorkItemsResponse> GetAllBacklogWorkItems();
     }
 }
