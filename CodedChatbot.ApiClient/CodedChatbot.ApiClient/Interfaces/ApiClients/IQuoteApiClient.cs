eb732b06e4236ac69041af02f223401dfd6453a0
codedghost2 <codedghost2@gmail.com>
2020-01-29 20:50:12 +0000

    Adding QuoteApi Client

---
+++ CodedChatbot.ApiClient/CodedChatbot.ApiClient/Interfaces/ApiClients/IQuoteApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CoreCodedChatbot.ApiContract.RequestModels.Quotes;
+using CoreCodedChatbot.ApiContract.ResponseModels.Quotes;
+
+namespace CoreCodedChatbot.ApiClient.Interfaces.ApiClients
+{
+    public interface IQuoteApiClient
+    {
+        Task<AddQuoteResponse> AddQuote(AddQuoteRequest request);
+        Task<bool> EditQuote(EditQuoteRequest request);
+        Task<bool> RemoveQuote(RemoveQuoteRequest request);
+        Task<GetQuoteResponse> GetQuote(GetQuoteRequest request);
+    }
+}
