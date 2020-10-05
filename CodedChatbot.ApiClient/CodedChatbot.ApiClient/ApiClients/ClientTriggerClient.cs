47dcee5233de95e06b8cedb37345e592c542adc8
codedghost2 <codedghost2@gmail.com>
2020-10-05 23:04:29 +0100

    Adding client trigger endpoint client

---
+++ CodedChatbot.ApiClient/CodedChatbot.ApiClient/ApiClients/ClientTriggerClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CoreCodedChatbot.ApiClient.DataHelper;
+using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.ApiContract.RequestModels.ClientTrigger;
+using CoreCodedChatbot.Config;
+using CoreCodedChatbot.Secrets;
+using Microsoft.Extensions.Logging;
+
+namespace CoreCodedChatbot.ApiClient.ApiClients
+{
+    public class ClientTriggerClient : IClientTriggerClient
+    {
+        private readonly ILogger<IClientTriggerClient> _logger;
+        private HttpClient _clientTriggerClient;
+
+        public ClientTriggerClient(
+            IConfigService configService,
+            ISecretService secretService,
+            ILogger<IClientTriggerClient> logger
+        )
+        {
+            _logger = logger;
+            _clientTriggerClient = HttpClientHelper.BuildClient(configService, secretService, "ClientTrigger");
+        }
+        public async Task<bool> CheckBackgroundSong(CheckBackgroundSongRequest request)
+        {
+            try
+            {
+                var result =
+                    await _clientTriggerClient.PostAsync("CheckBackgroundSong", HttpClientHelper.GetJsonData(request));
+
+                return result.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                return HttpClientHelper.LogError<bool>(_logger, e, new object[] { request.Username });
+            }
+        }
+    }
+}
---
+++ CodedChatbot.ApiClient/CodedChatbot.ApiClient/Interfaces/ApiClients/IClientTriggerClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CoreCodedChatbot.ApiContract.RequestModels.ClientTrigger;
+
+namespace CoreCodedChatbot.ApiClient.Interfaces.ApiClients
+{
+    public interface IClientTriggerClient
+    {
+        Task<bool> CheckBackgroundSong(CheckBackgroundSongRequest request);
+    }
+}
