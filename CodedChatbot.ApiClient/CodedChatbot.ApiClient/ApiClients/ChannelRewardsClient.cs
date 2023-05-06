d75b9daea1eb7bf8b8541eabdc9e43cdbcc17ad5
codedghost2 <codedghost2@gmail.com>
2023-05-06 23:34:21 +0100

    Updating client

--- CodedChatbot.ApiClient/CodedChatbot.ApiClient/ApiClients/ChannelRewardsClient.cs
+++ CodedChatbot.ApiClient/CodedChatbot.ApiClient/ApiClients/ChannelRewardsClient.cs
@@ -3,7 +3,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using CoreCodedChatbot.ApiClient.DataHelper;
 using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.ApiContract.Enums.ChannelRewards;
 using CoreCodedChatbot.ApiContract.RequestModels.ChannelRewards;
+using CoreCodedChatbot.ApiContract.ResponseModels.ChannelRewards;
 using CoreCodedChatbot.Config;
 using CoreCodedChatbot.Secrets;
 using Microsoft.Extensions.Logging;
@@ -41,18 +43,18 @@ namespace CoreCodedChatbot.ApiClient.ApiClients
             }
         }
 
-        public async Task<bool> StoreRedemption(StoreRewardRedemptionRequest request)
+        public async Task<StoreRedemptionResponse> StoreRedemption(StoreRewardRedemptionRequest request)
         {
             try
             {
                 var result =
                     await _channelRewardsClient.PostAsync("StoreRedemption", HttpClientHelper.GetJsonData(request));
 
-                return result.IsSuccessStatusCode && JsonConvert.DeserializeObject<bool>(await result.Content.ReadAsStringAsync());
+                return JsonConvert.DeserializeObject<StoreRedemptionResponse>(await result.Content.ReadAsStringAsync());
             }
             catch (Exception e)
             {
-                return HttpClientHelper.LogError<bool>(_logger, e, new object[] {request.ChannelRewardId, request.RedeemedBy});
+                return HttpClientHelper.LogError<StoreRedemptionResponse>(_logger, e, new object[] {request.ChannelRewardId, request.RedeemedBy});
             }
         }
     }
--- CodedChatbot.ApiClient/CodedChatbot.ApiClient/Interfaces/ApiClients/IChannelRewardsClient.cs
+++ CodedChatbot.ApiClient/CodedChatbot.ApiClient/Interfaces/ApiClients/IChannelRewardsClient.cs
@@ -1,11 +1,12 @@
 ﻿using System.Threading.Tasks;
 using CoreCodedChatbot.ApiContract.RequestModels.ChannelRewards;
+using CoreCodedChatbot.ApiContract.ResponseModels.ChannelRewards;
 
 namespace CoreCodedChatbot.ApiClient.Interfaces.ApiClients
 {
     public interface IChannelRewardsClient
     {
         Task<bool> CreateOrUpdate(CreateOrUpdateChannelRewardRequest request);
-        Task<bool> StoreRedemption(StoreRewardRedemptionRequest request);
+        Task<StoreRedemptionResponse> StoreRedemption(StoreRewardRedemptionRequest request);
     }
 }
